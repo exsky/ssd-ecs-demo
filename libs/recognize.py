8c7d146260e81b59a5f6d3e5b211b4176a8bb1e2
Alex Yang <sky@nipapa.tw>
2021-11-11 18:22:54 +0800

    fix: correct typo and paras

--- libs/recognize.py
+++ libs/recognize.py
@@ -9,12 +9,19 @@ from collections import namedtuple
 
 
 class CHTVisu:
+    pic_size = 0
+    confidence_threshold = 0
+    input_shapes = None
+    candidates = None
+    mod = None
+    batch = None
 
     def __init__(self):
-        SHAPE = 512
-        input_shapes=[('data', (1, 3, SHAPE, SHAPE))]
-        confidence_threshold = 0.2
-        CLASSES = ['changty','sky','roger','jimmy','kfira','rachael','rinns','tclan']
+        self.pic_size = 512
+        self.confidence_threshold = 0.2
+        self.input_shapes = [('data', (1, 3, self.pic_size, self.pic_size))]
+        # CLASSES = ['changty','sky','roger','jimmy','kfira','rachael','rinns','tclan']
+        self.candidates = ['changty','sky','roger','jimmy','kfira','rachael','rinns','tclan']
 
     def get_ctx(self):
         try:
@@ -32,38 +39,42 @@ class CHTVisu:
     def justify(self, j_image):
         ctx = self.get_ctx()
         # Load Module
-        param_path = os.path.join(TMP_FOLDER, 'ssd_vgg16_512')
+        param_path = os.path.join('trained-model/', 'ssd_vgg16_512')
         # print("param_path: {}".format(param_path))
         sym, arg_params, aux_params = mx.model.load_checkpoint(param_path, 0)
-        mod = mx.mod.Module(symbol=sym, label_names=[], context=ctx)
-        mod.bind(for_training=False, data_shapes=input_shapes)
-        mod.set_params(arg_params, aux_params)
-        Batch = namedtuple('Batch', ['data'])
+        self.mod = mx.mod.Module(symbol=sym, label_names=[], context=ctx)
+        self.mod.bind(for_training=False, data_shapes=self.input_shapes)
+        self.mod.set_params(arg_params, aux_params)
+        self.batch = namedtuple('Batch', ['data'])
+        print('Load image ...')
         if not j_image:
             j_image = 'images/line_637669196787939.jpg'
-        results, org_image = infer(j_image)
-        detected_guy = CLASSES[int(results[0][0]))
+        results, org_image = self.infer(j_image)
+        detected_guy = self.candidates[int(results[0][0])]
+        print(detected_guy)
         return detected_guy
 
-    def predict_from_file(self, filepath, reshape=(SHAPE, SHAPE)):
+    def predict_from_file(self, filepath):
         # Switch RGB to BGR format (which ImageNet networks take)
         img = cv2.cvtColor(cv2.imread(filepath), cv2.COLOR_BGR2RGB)
         if img is None:
             return []
-         # Resize image to fit network input
+        # Resize image to fit network input
+        reshape = (self.pic_size, self.pic_size)
         img = cv2.resize(img, reshape)
         org_image = img.copy()
         img = np.swapaxes(img, 0, 2)
         img = np.swapaxes(img, 1, 2)
         img = img[np.newaxis, :]
-        mod.forward(Batch([mx.nd.array(img)]))
-        prob = mod.get_outputs()[0].asnumpy()
+        self.mod.forward(self.batch([mx.nd.array(img)]))
+        prob = self.mod.get_outputs()[0].asnumpy()
         prob = np.squeeze(prob)
         return prob, org_image
 
-    def infer(self, image_path, threshold=confidence_threshold):
+    def infer(self, image_path):
+        threshold = self.confidence_threshold
         results, org_image = self.predict_from_file(image_path)
         image_name = image_path.split("/")[-1]
         filtered_result = results[results[:, 0] != -1]
-        filtered_result = filtered_result[filtered_result[:, 1] >=threshold]
+        filtered_result = filtered_result[filtered_result[:, 1] >= threshold]
         return filtered_result, org_image
