82e9278c02475a4a89ef1c95728cca91fee03723
exsky <sky@nipapa.tw>
2021-11-13 15:56:01 +0800

    feat: fetch url in parallel

--- libs/horoscope.py
+++ libs/horoscope.py
@@ -1,29 +1,34 @@
+import aiohttp
+import asyncio
 import requests
 from bs4 import BeautifulSoup
 
 
-def getFortune(what=None):
+async def getFortune(what, session):
     if not what:
         url = "https://www.daily-zodiac.com/mobile/zodiac/Aries"
     else:
         url = "https://www.daily-zodiac.com/mobile/zodiac/{}".format(what)
     try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        result = soup.find("article").getText()
-        result = result.replace('\n', '')
-        result = result.replace(' ', '')
+        async with session.get(url=url) as response:
+            resp = await response.read()
+            soup = BeautifulSoup(resp, "html.parser")
+            result = soup.find("article").getText()
+            result = result.replace('\n', '')
+            result = result.replace(' ', '')
     except Error as e:
         print(e)
     return result
 
 
-def get_12_horo():
+async def get_12_horo():
     fortunes = {}
     zodiac = [ 'Aries', 'Taurus', 'Gemini', 'Cancer',
                'Leo', 'Virgo', 'Libra', 'Scorpio',
                'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces'
                ]
-    for z in zodiac:
-        fortunes[z] = getFortune(z)
+    async with aiohttp.ClientSession() as session:
+        ret = await asyncio.gather(*[getFortune(z, session) for z in zodiac])
+        for z in zodiac:
+            fortunes[z] = ret[zodiac.index(z)]
     return fortunes
