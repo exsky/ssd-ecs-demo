05e1e231e72b54c4154d66c9300958106326b2d4
exsky <sky@nipapa.tw>
2022-02-23 09:58:12 +0800

    :feat: capture picture from cam

---
+++ capture.py
@@ -0,0 +1,47 @@
+import cv2
+
+def capture_from_cam(num=None):
+    if not num:
+        num = 0
+    try:
+        camera = cv2.VideoCapture(num)
+        (grabbed, frame) = camera.read()
+    except Exception as e:
+        print(e)
+
+def record_from_cam(num=None):
+    if not num:
+        num = 0
+    try:
+        cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    except Exception as e:
+        print(e)
+
+    # 使用 XVID encoding
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+
+    while cap.isOpened():
+        (grabbed, frame) = cap.read()
+        if not grabbed:
+            print('Stop grabbing ...')
+            break
+        out.write(frame)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+def main():
+    record_from_cam()
+    #while True:
+    #    capture_from_cam()
+
+if __name__ == '__main__':
+    main()
